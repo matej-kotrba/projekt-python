616564b96e77d9683025ec2f5a4e1c576eaabe78
unknown <dokonala.hilda@gmail.com>
2020-10-12 08:14:56 +0200

    Fundamentals

---
+++ 02-fundamentals/physics.py
@@ -0,0 +1,22 @@
+'''
+Konstanty v Pythonu
+
+Konstanta je vlastně speciální typ proměnné, jejíž hodnota nemůže být změněna.
+V Pythonu jsou konstanty obvykle deklarovány a přiřazovány v modulu, který bývá importován do souboru aplikace.
+Konstanty jsou pojmenovány velkými písmeny a jednotlivá slova jsou oddělována podtržítky.
+'''
+
+EARTH_GRAVITY = 0 #? normální pozemské tíhové zrychlení
+MOON_GRAVITY = 0 #? měsíční gravitace
+SPEED_OF_LIGHT = 0 #? rychlost světla ve vakuu
+SPEED_OF_SOUND = 0 #? rychlost zvuku při teplotě 20 °C v suchém vzduchu
+
+''' 
+Úkol:
+1. Doplňte správně hodnoty uvedených konstant.
+2. Doplňte physics.py o několik výpočtových funkcí (opatřené docstrings), v nichž využijete minimálně všechny výše uvedené konstanty.
+Samozřejmě můžete své řešení rozšířit i o jiné fyzikální konstanty.
+3. Vytvořte z tohoto souboru samostatný modul v Pythonu podle návodu, který si sami najdete na internetu.      
+4. Vytvořte vlastní aplikaci myapp.py, do níž tento modul importujte. Demonstrujte v ní na jednoduchých příkladech využití vámi
+připravených funkcí.  
+'''
