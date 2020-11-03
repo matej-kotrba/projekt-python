edfa4a55c3fc2d9daa0bc42716f97dce73ea58db
unknown <dokonala.hilda@gmail.com>
2020-11-03 08:44:47 +0100

    Úvod do OOP v jazyce Python - 2 - hotová třída

--- 04-oop-intro/point.py
+++ 04-oop-intro/point.py
@@ -14,7 +14,7 @@ class Point:
         self.x = x
         self.y = y
         #? Vytvořte atribut objektu color a přiřaďte mu výchozí barvu podle výchozího atributu třídy
-
+        self.color = Point.default_color
 
     # Magická metoda pro výpis textové informace o objektu
     def __str__(self):
@@ -35,8 +35,14 @@ class Point:
         return Point(self.x + other.x, self.y + other.y)
 
     #? Vytvoř magickou metodu, která umožní rozdílem dvou objektů-bodů (jejich odečtením) zjistit jejich vzdálenost
+    def __sub__(self, other):
+        # Provede zjištění vzdálenosti dvou objektů
+        return (abs(self.x - other.x) ** 2 + abs(self.y - other.y) ** 2) ** 0.5
 
     #? Tady bude statická (třídní) metoda random_color(), která vygeneruje náhodou barvu rgb zapsanou hexadecimálně
+    @staticmethod
+    def random_color():
+        return '#' + ''.join([str(hex(random.randint(0,255)))[2:] for i in range(3)])
 
     # Metoda třídy, která vytvoří nový objekt-bod na nulových pozicích
     @classmethod
@@ -46,6 +52,9 @@ class Point:
 
     #? Tady bude metoda třídy random_pos(), která bude na základě zadaných argumentů určujících platný rozsah hodnot
     #? vytvářet náhodně umístěné body
+    @classmethod
+    def random_pos(cls, min=0, max=10):
+        return cls(random.randrange(min, max), random.randrange(min, max))
 
     # Metoda objektu, která (symbolicky v podobě textu) "vykreslí" daný objekt
     def draw(self):
@@ -53,6 +62,8 @@ class Point:
         print(f'Bod (x:{self.x}, y:{self.y}), barva: {self.color}')
 
     #? Tady bude metoda objektu change_color(), která bude na základě zadaného argumentu nastavovat barvu objektu
+    def change_color(self, color):
+        self.color = color
 
 
 #? Vytvoř objekt bod1 na pozici x: 8, y: 5
